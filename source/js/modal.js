a33401de7ff997a89792bb4f6a46a1796aa817ef
Наталья Зиновьева <natalizin85@gmail.com>
2023-04-07 20:11:53 +1000

    правка ошибок по stylelint

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,37 +1,33 @@
-const modalContainer = document.querySelector(".modal");
-const modalCloseButton = document.querySelector('.modal__button');
+  const modalButtons = document.querySelectorAll('.modal-button');
+  const modal = document.querySelector('.modal');
+  const modalCloseButton = document.querySelector('.modal__button');
+  const modalFormInput = document.querySelector('.modal__form-group-input');
 
-const openModal = () => {
-  modalContainer.classList.add("is-open");
-  document.addEventListener("keydown", onDocumentKeydown);
-  modalContainer.addEventListener("click", onModalClick);
-}
+  const onModalButtonClick = (evt) => {
+    evt.preventDefault();
+    modal.classList.add('is-open');
+    modalFormInput.focus();
+  };
 
-const closeModal = () => {
-  modalContainer.classList.remove("is-open");
-  document.removeEventListener("keydown", onDocumentKeydown);
-  modalContainer.removeEventListener("click", onModalClick);
-}
+  const onModalCloseButtonClick = (evt) => {
+    evt.preventDefault();
+    modal.classList.remove('is-open');
+  };
 
-modalCloseButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  modalContainer.classList.remove('is-open');
-});
+  const onModalClick = (evt) => {
+    if (evt.target === modal) {
+      modal.classList.remove("is-open");
+    }
+  };
 
-document.addEventListener('click', (evt) => {
-  if (evt.target.closest('.button__modal')) {
-    openModal();
-  }
-});
+  const onDocumentKeydown = (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      modal.classList.remove('is-open');
+    }
+  };
 
-const onDocumentKeydown = (evt) => {
-  if(evt.key === 'Escape') {
-    closeModal();
-  }
-};
-
-const onModalClick = (evt) => {
-  if(!evt.target.closest('.modal__container')) {
-    closeModal();
-  }
-};
+  modalButtons.forEach((button) => button.addEventListener('click', onModalButtonClick));
+  modalCloseButton.addEventListener('click', onModalCloseButtonClick);
+  modal.addEventListener('click', onModalClick);
+  document.addEventListener('keydown', onDocumentKeydown);
